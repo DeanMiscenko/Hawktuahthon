f2be29250e8a2daf8808e905cd32b1eb53f1a878
CrunchyCherries <4075559@gmail.com>
2025-03-15 21:06:54 +1100

    Update clicker.py

--- Hackathon/clicker.py
+++ Hackathon/clicker.py
@@ -15,24 +15,26 @@ window_width,window_height = screen_width - 10,screen_height-50
 window = pygame.display.set_mode((window_width,window_height), pygame.RESIZABLE)
 pygame.display.set_caption(name)
 clickimg = pygame.image.load('click me.png')
+print(clickimg.get_size())
+
 class Button():
-    def __init__(self, x, y, image):
+    def __init__(self, y, image):
         self.image = image
         self.size = self.image.get_size()
-        self.width = x
         self.height = y
         self.clicked = False
 
-    def clicking(self, multiplier, surface, window_width, degree):
+        
+    def draw(self, multiplier, surface, window_width, degree):
         global handled
         if degree == 1:
             self.clicked = False
-        self.rect = pygame.Rect(self.width, self.height, self.size[0] * multiplier, self.size[1] * multiplier)
+        self.rect = pygame.Rect((window_width + 10 - (self.size[0] * multiplier)) / 2, self.height, self.size[0] * multiplier, self.size[1] * multiplier)
         pos = pygame.mouse.get_pos()
         counter = 1.05
-        if self.rect.collidepoint(pos):
+        if self.rect.collidepoint(pos): # DO NOT CHANGE THIS
             surface.blit(pygame.transform.scale(self.image, (self.size[0] * multiplier * counter, self.size[1] * multiplier * counter)), 
-                         (self.width - (self.rect[2] * (counter - 1) / 2), self.height - (self.rect[3] * (counter - 1)/ 2)))
+                         ((window_width + 10 - (self.size[0] * multiplier * counter)) / 2, self.height - (self.rect[3] * (counter - 1) / 2)))
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False and not handled:
                 self.clicked = True
                 handled = pygame.mouse.get_pressed()[0]
@@ -42,6 +44,45 @@ class Button():
                 pass
             else:
                 handled = False
+        
+        else:
+            surface.blit(pygame.transform.scale(self.image, (self.size[0] * multiplier, self.size[1] * multiplier)), 
+                         ((window_width + 10 - (self.size[0] * multiplier)) / 2, self.height))
+            if pygame.mouse.get_pressed()[0] == 1:
+                self.clicked = False
+
+        #print(self.clicked)
+        return self.clicked
+
+class Powerup():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.size = self.image.get_size()
+        self.width = x
+        self.height = y
+        self.clicked = False
+
+    def clicking(self, multiplier, surface, window_width, degree):
+        global handle
+        if degree == 1:
+            self.clicked = False
+        if degree == 2:
+            return True
+        self.rect = pygame.Rect(self.width, self.height, self.size[0] * multiplier, self.size[1] * multiplier)
+        pos = pygame.mouse.get_pos()
+        counter = 1.05
+        if self.rect.collidepoint(pos):
+            surface.blit(pygame.transform.scale(self.image, (self.size[0] * multiplier * counter, self.size[1] * multiplier * counter)), 
+                         (self.width - (self.rect[2] * (counter - 1) / 2), self.height - (self.rect[3] * (counter - 1)/ 2)))
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False and not handle:
+                self.clicked = True
+                handle = pygame.mouse.get_pressed()[0]
+                
+                print('clicked')
+            elif pygame.mouse.get_pressed()[0] == 1 and self.clicked == False and handle:
+                pass
+            else:
+                handle = False
         else:
             surface.blit(pygame.transform.scale(self.image, (self.size[0] * multiplier, self.size[1] * multiplier)), 
                          (self.width, self.height))
@@ -50,20 +91,30 @@ class Button():
 
         #print(self.clicked)
         return self.clicked
-click = Button(500, 200, clickimg)
+    
+font = pygame.font.SysFont('arial', 32)
+
+counter = 0
+text = font.render(f'Dopamine Points {counter}', True, (0, 0, 0), (255, 255, 255))
+click = Button(200, clickimg)
+dopamine = Button(270, text)
 run = True
 handled = False
-counter = 0
-while run:
-    print(counter)
-    window.fill(color='white')
-    if click.clicking(0.6, window, window_width, 1):
-        counter += 1
+handle = False
 
+while run:
+    dopamine = Button(270, text)
+    print(counter)
+    
+    window.fill(color='white')
+    
+    if click.draw(0.6, window, window_width, 1):
+        counter += 1
+    dopamine.draw(0.6, window, window_width, 2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
+    text = font.render(f'Dopamine Points {counter}', True, (0, 0, 0), (255, 255, 255))
     pygame.display.update()
     clock.tick(60)
     
