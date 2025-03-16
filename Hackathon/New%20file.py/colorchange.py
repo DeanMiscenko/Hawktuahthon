8aa7b296fea1d084751cbe0d1ad4924d5e66ceb5
PythonbetterthanHtml <noahisawesome313@gmail.com>
2025-03-16 14:29:07 +1100

    Update colorchange.py

--- Hackathon/New%20file.py/colorchange.py
+++ Hackathon/New%20file.py/colorchange.py
@@ -1,1 +1,21 @@
 
+    if colorcounter == 0: #THis cahnges the color
+            Circle_color = (255, 0, 0)
+            colorcounter += 1
+            pygame.time.get_ticks()
+        elif colorcounter == 1:
+            Circle_color = (0, 255, 0)
+            colorcounter += 1
+            pygame.time.get_ticks()
+        elif colorcounter == 2:
+            Circle_color = (0, 0, 255)
+            colorcounter = 0
+            pygame.time.get_ticks()
+
+ if powerup3.collidepoint(pos):
+                Color_change = True #This stars the code
+
+Circle_color = (255, 255 ,255) #Set this at the start
+colorcounter = 0
+pygame.draw.circle(Screen, Circle_color, (pos), 20 ) draws circle
+        
